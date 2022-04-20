e7c889376a5cbb2ded77cfae3aea28dfd41789e8
reactnetau <polyrhythmm@gmail.com>
2022-04-20 13:49:51 +1000

    build

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,6 @@ const Contact = () => {
       <h1>Contact</h1>
       <h3>Andrew Day</h3>
       <h4>Melbourne, Australia</h4>
-<<<<<<< HEAD
       <div className="contact-box">
         <h3>Linkedin</h3>
         <p>
@@ -34,44 +33,6 @@ const Contact = () => {
           <a href="https://github.com/reactnetau">github.com/reactnetau</a>
         </p>
       </div>
-=======
-      <Row>
-        <Col sm={12} md={6}>
-          <div className="contact-box-left">
-            <h3>Linkedin</h3>
-            <p>
-              <a href="https://www.linkedin.com/in/reactnetau">
-                linkedin.com/in/reactnetau
-              </a>
-            </p>
-          </div>
-        </Col>
-        <Col sm={12} md={6}>
-          <div className="contact-box-right">
-            <h3>Email</h3>
-            <p>
-              <a href="mailto:polyrhythmm@gmail.com">polyrhythmm@gmail.com</a>
-            </p>
-          </div>
-        </Col>
-        <Col sm={12} md={6}>
-          <div className="contact-box-left">
-            <h3>Mobile</h3>
-            <p>
-              <a href="tel:+61430744579">+61430744579</a>
-            </p>
-          </div>
-        </Col>
-        <Col sm={12} md={6}>
-          <div className="contact-box-right">
-            <h3>Github</h3>
-            <p>
-              <a href="https://github.com/reactnetau">github.com/reactnetau</a>
-            </p>
-          </div>
-        </Col>
-      </Row>
->>>>>>> d786d526d2b13d01d58c84737042376fde0b763f
     </div>
   );
 };
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -7,7 +7,6 @@ import { Row, Col } from 'react-bootstrap';
 
 const Details = () => {
   return (
-<<<<<<< HEAD
     <div className="grid container">
       <Detail
         title="Front End Development"
@@ -27,31 +26,6 @@ const Details = () => {
         back={Detail3}
       />
     </div>
-=======
-    <Row>
-      <Col xs={12} sm={12} md={4}>
-        <Detail
-          title="Front End Development"
-          description="React solutions using HTML, SCSS, Javascript, Typescript, NextJS."
-          back={Detail1}
-        />
-      </Col>
-      <Col xs={12} sm={12} md={4}>
-        <Detail
-          title="Back End Development"
-          description="Node, Express, MongoDB, Amazon Web Service, NextJS and GraphQL"
-          back={Detail2}
-        />
-      </Col>
-      <Col xs={12} sm={12} md={4}>
-        <Detail
-          title="UX/UI Design"
-          description="Clean and modern User Experience/User Interface designs"
-          back={Detail3}
-        />
-      </Col>
-    </Row>
->>>>>>> d786d526d2b13d01d58c84737042376fde0b763f
   );
 };
 
